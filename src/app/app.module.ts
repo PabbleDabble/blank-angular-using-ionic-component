5aa83e695bbd99c582457228a21e62015ab6f5ff
PatrickVielhaber <Patrick.Vielhaber@streamlinx.com>
2023-08-31 11:07:32 -0400

    $ ng generate component myComp --style=scss --skip-tests=true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyCompComponent } from './my-comp/my-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCompComponent
   ],
   imports: [
     BrowserModule
